684fc6be660b062b92492b0ab85954d1c963cfbb
eldar.bajrovic <eldar.bajrovic@gmail.com>
2023-02-15 08:22:37 +0100

    Insert sql bat file in SQL bak file folder.

--- ScrapeInfoTask/Controllers/HomeController.cs
+++ ScrapeInfoTask/Controllers/HomeController.cs
@@ -41,49 +41,6 @@ namespace ScrapeInfoTask.Controllers
 
         public IActionResult Converter()
         {
-            //string fileName = Path.GetTempFileName();
-            //file.SaveAs(fileName);
-
-            //string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties=Excel 12.0;";
-            //OleDbConnection connection = new OleDbConnection(connectionString);
-            //connection.Open();
-
-            //System.Data.DataTable dt = new System.Data.DataTable();
-            //OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM [Sheet1$]", connection);
-            //adapter.Fill(dt);
-
-            //connection.Close();
-
-            //Application wordApp = new Application();
-            //Document wordDoc = wordApp.Documents.Add();
-
-            //Table table = wordDoc.Tables.Add(wordDoc.Range(), dt.Rows.Count + 1, dt.Columns.Count);
-
-            //// Add header row
-            //for (int i = 0; i < dt.Columns.Count; i++)
-            //{
-            //    table.Rows[1].Cells[i + 1].Range.Text = dt.Columns[i].ColumnName;
-            //}
-
-            //// Add data rows
-            //for (int i = 0; i < dt.Rows.Count; i++)
-            //{
-            //    for (int j = 0; j < dt.Columns.Count; j++)
-            //    {
-            //        table.Rows[i + 2].Cells[j + 1].Range.Text = dt.Rows[i][j].ToString();
-            //    }
-            //}
-
-            //string savePath = Path.ChangeExtension(fileName, ".docx");
-            //wordDoc.SaveAs2(savePath);
-            //wordDoc.Close();
-            //wordApp.Quit();
-
-            //byte[] fileBytes = System.IO.File.ReadAllBytes(savePath);
-            //return File(fileBytes, "application/msword", "ExcelToWord.docx");
-
-
-
             return View();
         }
 
